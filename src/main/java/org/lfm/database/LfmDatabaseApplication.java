a8f15221d6ed52282b4f73a2372bc2586c924511
maxey <emgireyes17@gmail.com>
2026-08-29 11:33:10 +0800

    add Spring Boot entry point

---
+++ src/main/java/org/lfm/database/LfmDatabaseApplication.java
@@ -0,0 +1,12 @@
+package org.lfm.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LfmDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LfmDatabaseApplication.class, args);
+    }
+}
