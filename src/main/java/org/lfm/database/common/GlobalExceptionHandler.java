0f7e2cd523ba8282ab8f400e5a871887fd7392f9
maxey <emgireyes17@gmail.com>
2026-09-02 13:11:01 +0800

    return a 404 page for a missing person

---
+++ src/main/java/org/lfm/database/common/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package org.lfm.database.common;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class GlobalExceptionHandler
+{
+    @ExceptionHandler(PersonNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String personNotFound()
+    {
+        return "error/not-found";
+    }
+}
